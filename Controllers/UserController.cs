5e9581bde897473117202ad05ad934d6dc388956
Fischdaling <gabrielrokyta@gmail.com>
2025-05-13 18:15:49 +0200

    init

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchuelerChatBackendProject.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+	
+}
---
+++ Model/Address.cs
@@ -0,0 +1,3 @@
+﻿namespace SchuelerChatBackendProject;
+
+public record Address(string Strasse, int Hausnummer, string Posleitzahl, string Postort);
